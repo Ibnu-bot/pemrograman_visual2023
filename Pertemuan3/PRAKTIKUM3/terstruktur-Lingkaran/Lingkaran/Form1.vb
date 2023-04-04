347ed301538c373578f65e5d7fc64a8e47fe0559
Ibnu Hasan <103122181+Ibnu-bot@users.noreply.github.com>
2023-04-04 14:48:01 +0700

    Add files via upload

---
+++ Pertemuan3/PRAKTIKUM3/terstruktur-Lingkaran/Lingkaran/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Function HitungLuas(jari As Integer) As Integer
+        Dim luas As Integer
+        luas = 22 / 7 * (jari * jari)
+        Return luas
+    End Function
+
+    Private Function HitungKeliling(jari As Integer) As Integer
+        Dim keliling As Integer
+        keliling = 2 * 22 / 7 * jari
+        Return keliling
+    End Function
+
+    Private Sub btnhitung_Click(sender As Object, e As EventArgs) Handles btnhitung.Click
+        Dim jari, luas, keliling As Integer
+
+        jari = Val(txtjari.Text)
+
+        luas = HitungLuas(jari)
+        keliling = HitungKeliling(jari)
+
+        txtluas.Text = Str(luas)
+        txtkeliling.Text = Str(keliling)
+
+    End Sub
+End Class
