6d9b56b264e3617ac45560ba451b67bcf906710c
Ibnu Hasan <103122181+Ibnu-bot@users.noreply.github.com>
2023-04-04 14:43:59 +0700

    Add files via upload

---
+++ Pertemuan3/PRAKTIKUM3/terstruktur-jajar genjang/jajar genjang/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Function HitungLuas(a As Integer, t As Integer) As Integer
+        Dim luas As Integer
+        luas = a * t
+        Return luas
+    End Function
+
+    Private Function HitungKeliling(a As Integer, b As Integer) As Integer
+        Dim keliling As Integer
+        keliling = 2 * (a + b)
+        Return keliling
+    End Function
+
+    Private Sub btnhitung_Click(sender As Object, e As EventArgs) Handles btnhitung.Click
+        Dim a, t, b, luas, keliling As Integer
+
+        a = Val(txtalas.Text)
+        t = Val(txttinggi.Text)
+        b = Val(txtsisimiring.Text)
+
+        luas = HitungLuas(a, t)
+        keliling = HitungKeliling(a, b)
+
+        txtluas.Text = Str(luas)
+        txtkeliling.Text = Str(keliling)
+
+    End Sub
+End Class
