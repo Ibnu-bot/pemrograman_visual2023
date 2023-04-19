9a8134dcbc82da6508309e047f1bc58e7cd5f02d
Ibnu Hasan <103122181+Ibnu-bot@users.noreply.github.com>
2023-04-19 15:05:43 +0700

    Add files via upload

---
+++ Pertemuan5/praktikum 5/praktikum select case 1/praktikum select case 1/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Select Case txtplat.Text
+            Case "A" : txtketerangan.Text = "Banten"
+
+            Case "B" : txtketerangan.Text = "Jakarta"
+
+            Case "D" : txtketerangan.Text = "Bandung"
+
+            Case "E" : txtketerangan.Text = "Cirebon"
+
+            Case Else : txtketerangan.Text = "Plat belum terdaftar"
+        End Select
+    End Sub
+End Class
---
+++ Pertemuan5/praktikum 5/praktikum select case 2/praktikum select case 2/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Select Case txtpabrik.Text
+            Case "TMMIN" : txtketerangan.Text = "Toyota Motor Manufacturing Indonesia"
+
+            Case "AHM" : txtketerangan.Text = "Astra Hpnda Motor"
+
+            Case "ADM" : txtketerangan.Text = "Astea Daihatsu Motor"
+
+            Case "HMMI" : txtketerangan.Text = "Hyundai Motor Manufacturing Indonesia"
+
+            Case Else : txtketerangan.Text = "apaan Tuh"
+        End Select
+    End Sub
+End Class
