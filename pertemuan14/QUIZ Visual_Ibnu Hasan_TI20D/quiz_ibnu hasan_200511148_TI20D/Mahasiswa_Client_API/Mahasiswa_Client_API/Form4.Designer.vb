fbd43a2e8642129ba119a65ee8b80165b22a421a
Ibnu Hasan <103122181+Ibnu-bot@users.noreply.github.com>
2023-07-31 10:04:36 +0700

    Add files via upload

---
+++ pertemuan14/QUIZ Visual_Ibnu Hasan_TI20D/quiz_ibnu hasan_200511148_TI20D/Mahasiswa_Client_API/Mahasiswa_Client_API/Form4.Designer.vb
@@ -0,0 +1,260 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form4
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.txtTgl = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtJK = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtNRP = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.dgvData = New System.Windows.Forms.DataGridView()
+        Me.txtAlamat = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtKota = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(632, 195)
+        Me.btnDelete.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(141, 40)
+        Me.btnDelete.TabIndex = 38
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSubmit.Location = New System.Drawing.Point(632, 91)
+        Me.btnSubmit.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(141, 40)
+        Me.btnSubmit.TabIndex = 37
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(632, 141)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(141, 40)
+        Me.btnClear.TabIndex = 36
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'txtTgl
+        '
+        Me.txtTgl.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTgl.Location = New System.Drawing.Point(332, 250)
+        Me.txtTgl.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtTgl.Name = "txtTgl"
+        Me.txtTgl.Size = New System.Drawing.Size(223, 28)
+        Me.txtTgl.TabIndex = 35
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(184, 256)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(121, 22)
+        Me.Label5.TabIndex = 34
+        Me.Label5.Text = "Tanggal Lahir"
+        '
+        'txtJK
+        '
+        Me.txtJK.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtJK.Location = New System.Drawing.Point(332, 200)
+        Me.txtJK.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtJK.Name = "txtJK"
+        Me.txtJK.Size = New System.Drawing.Size(223, 28)
+        Me.txtJK.TabIndex = 33
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(184, 200)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(121, 22)
+        Me.Label4.TabIndex = 32
+        Me.Label4.Text = "Jenis Kelamin"
+        '
+        'txtNama
+        '
+        Me.txtNama.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNama.Location = New System.Drawing.Point(332, 146)
+        Me.txtNama.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(223, 28)
+        Me.txtNama.TabIndex = 31
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(184, 146)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 22)
+        Me.Label3.TabIndex = 30
+        Me.Label3.Text = "Nama"
+        '
+        'txtNRP
+        '
+        Me.txtNRP.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNRP.Location = New System.Drawing.Point(332, 96)
+        Me.txtNRP.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtNRP.Name = "txtNRP"
+        Me.txtNRP.Size = New System.Drawing.Size(223, 28)
+        Me.txtNRP.TabIndex = 29
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(184, 96)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 22)
+        Me.Label2.TabIndex = 28
+        Me.Label2.Text = "NRP"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(327, 55)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 26)
+        Me.Label1.TabIndex = 27
+        '
+        'dgvData
+        '
+        Me.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvData.Location = New System.Drawing.Point(188, 400)
+        Me.dgvData.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.dgvData.Name = "dgvData"
+        Me.dgvData.RowTemplate.Height = 24
+        Me.dgvData.Size = New System.Drawing.Size(897, 304)
+        Me.dgvData.TabIndex = 26
+        '
+        'txtAlamat
+        '
+        Me.txtAlamat.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAlamat.Location = New System.Drawing.Point(332, 301)
+        Me.txtAlamat.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtAlamat.Name = "txtAlamat"
+        Me.txtAlamat.Size = New System.Drawing.Size(223, 28)
+        Me.txtAlamat.TabIndex = 40
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(184, 305)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(65, 22)
+        Me.Label6.TabIndex = 39
+        Me.Label6.Text = "Alamat"
+        '
+        'txtKota
+        '
+        Me.txtKota.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtKota.Location = New System.Drawing.Point(332, 349)
+        Me.txtKota.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtKota.Name = "txtKota"
+        Me.txtKota.Size = New System.Drawing.Size(223, 28)
+        Me.txtKota.TabIndex = 42
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(184, 349)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(47, 22)
+        Me.Label7.TabIndex = 41
+        Me.Label7.Text = "Kota"
+        '
+        'Form4
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.PeachPuff
+        Me.ClientSize = New System.Drawing.Size(1122, 814)
+        Me.Controls.Add(Me.txtKota)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtAlamat)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.txtTgl)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtJK)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtNRP)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dgvData)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form4"
+        Me.Text = "Form4"
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents txtTgl As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtJK As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNRP As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents dgvData As DataGridView
+    Friend WithEvents txtAlamat As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtKota As TextBox
+    Friend WithEvents Label7 As Label
+End Class
